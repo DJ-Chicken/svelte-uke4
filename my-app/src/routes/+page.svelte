82cc08d7b954b8cd905f0ef39c6cf4ac688ddbad
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-01-24 09:11:48 +0100

    Tailwind

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -8,13 +8,14 @@
     }
 </script>
 
-
-<div>
-
-    <h1> OVEN </h1>
-    <p> Ovnen er {counter} grader </p>
-    
-    <button on:click={increaseCounter}> + </button>
-    <button on:click={decreaseCounter}> - </button>
-
+<div class="flex min-h-screen justify-center items-center w-full">
+    <div class="flex  flex-col justify-center items-center max-w-xs w-full">
+        
+            <h1> OVEN </h1>
+            <p> Ovnen er {counter} grader </p>
+            
+            <button on:click={increaseCounter}> + </button>
+            <button on:click={decreaseCounter}> - </button>
+        
+    </div>
 </div>
