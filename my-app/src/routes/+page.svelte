78a0e168cce9be7a23923d6d38f0f073eae8b8ec
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-01-24 10:57:14 +0100

    Knapper

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let counter = 0
+
+
+    function increaseCounter(){
+        counter += 1
+    }
+    function decreaseCounter(){
+        if (counter > 0) {
+            counter -= 1
+        }
+    }
+    
+</script>
+
+
+<div class="flex min-h-screen justify-center items-center w-full">
+    <div class="flex justify-center items-center max-w-xs w-full">
+        <button on:click={increaseCounter} class="flex justify-center items-center w-9 h-9 border rounded-full m-auto"> + </button>
+        <p class="flex justify-center items-center w-3/5 "> {counter}° </p>
+        <button on:click={decreaseCounter} class="flex justify-center items-center w-9 h-9 border rounded-full m-auto"> - </button>
+    </div>
+</div>
