7208a50df9465a9ba3c34b4e94908a7648dbcd84
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-01-24 08:58:09 +0100

    OVN

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let counter = 0
+    function increaseCounter(){
+        counter += 1
+    }
+    function decreaseCounter(){
+        counter -= 1
+    }
+</script>
+
+
+<div>
+
+    <h1> OVEN </h1>
+    <p> Ovnen er {counter} grader </p>
+    
+    <button on:click={increaseCounter}> + </button>
+    <button on:click={decreaseCounter}> - </button>
+
+</div>
