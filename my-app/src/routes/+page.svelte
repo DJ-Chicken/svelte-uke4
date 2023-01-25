bc33464a739bf8277e67f7554608258231b9031c
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-01-25 09:33:42 +0100

    Functions done

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,23 +1,39 @@
 <script>
-    let counter = 0
-
-
-    function increaseCounter(){
-        counter += 1
-    }
-    function decreaseCounter(){
-        if (counter > 0) {
-            counter -= 1
-        }
-    }
     
+    let counter = 0;
+	let holdingDown = false;
+
+	function increment() {
+		counter++;
+		if (holdingDown) {
+			setTimeout(increment, 15);
+		}
+	}
+    function decrement() {
+        if (counter > 0) {
+            counter--;
+            if (holdingDown) {
+                setTimeout(decrement, 15);
+            }   
+        }
+	}
+
 </script>
 
 
 <div class="flex min-h-screen justify-center items-center w-full">
     <div class="flex justify-center items-center max-w-xs w-full">
-        <button on:click={increaseCounter} class="flex justify-center items-center w-9 h-9 border rounded-full m-auto"> + </button>
+        
+        <button on:pointerdown={() => {
+            holdingDown = true; decrement();
+        }} on:pointerup={() => (holdingDown = false)} class="flex justify-center items-center w-9 h-9 border rounded-full m-auto"> - </button>
+
         <p class="flex justify-center items-center w-3/5 "> {counter}° </p>
-        <button on:click={decreaseCounter} class="flex justify-center items-center w-9 h-9 border rounded-full m-auto"> - </button>
+        
+        <button on:pointerdown={() => {
+            holdingDown = true; increment();
+        }} on:pointerup={() => (holdingDown = false)} class="flex justify-center items-center w-9 h-9 border rounded-full m-auto"> + </button>
+    
+    
     </div>
 </div>
